b5b92189fe96fe5101d7e5f1aa996eef8e5a3333
Luva Pacher <pacher@to.infn.it>
2016-04-04 01:32:49 +0200

    Simulation Tcl script skeleton

--- fpga/common/simulation/scripts/run.tcl
+++ fpga/common/simulation/scripts/run.tcl
@@ -1,1 +1,32 @@
+#-----------------------------------------------------------------------------------------------------
+#                               University of Torino -Department of Physics
+#                                   via Giuria 1 10125, Torino, Italy
+#-----------------------------------------------------------------------------------------------------
+# [File name]      run.tcl
+# [Project]        Adavanced Elecrtonics Laboratory course
+# [Author]         Luca Pacher - pacher@to.infn.it
+# [Language]       Tcl/Xilinx Xsim Tcl simulation commands
+# [Created]        Apr 04, 2016
+# [Modified]       Apr 04, 2016
+# [Description]    Sample simulation commands for the Xsim simulator
+# [Notes]          -
+# [Version]        1.0
+# [Revisions]      04.04.2015 - Created
+#-----------------------------------------------------------------------------------------------------
+
+
+## choose which signals will be plotted in the waveform window
+# wave_add ...
+
+
+## run the entire simulatio until a $finish or a $stop statement is encountered in testbench
 run all
+
+
+## run the simulation for a certain amoutn of time
+#run 720ns
+#run 2.5us
+
+
+## reset the simulation to t=0
+#restart
