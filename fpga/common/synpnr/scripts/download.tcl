8ce7ad8876d264897653a629d60f650594d6dffc
Luva Pacher <pacher@to.infn.it>
2016-04-04 02:05:20 +0200

    Makefiles for syn/pnr flow

--- fpga/common/synpnr/scripts/download.tcl
+++ fpga/common/synpnr/scripts/download.tcl
@@ -2,31 +2,16 @@
 #                               University of Torino - Department of Physics
 #                                   via Giuria 1 10125, Torino, Italy
 #-----------------------------------------------------------------------------------------------------
-# [File name]      run.tcl
+# [File name]      download.tcl
 # [Project]        Adavanced Electronics Laboratory course
 # [Author]         Luca Pacher - pacher@to.infn.it
 # [Language]       Tcl/Xilinx Vivado Tcl commands
-# [Created]        Apr 04, 2016
-# [Modified]       Apr 04, 2016
-# [Description]    Sample simulation commands for the Xsim simulator
+# [Created]        Mar 23, 2016
+# [Modified]       Mar 29, 2016
+# [Description]    Standalone bitstream download script using Xilinx Vivado Hardware Manager Tcl commands
 # [Notes]          -
 # [Version]        1.0
-# [Revisions]      04.04.2016 - Created
+# [Revisions]      23.03.2016 - Created
 #-----------------------------------------------------------------------------------------------------
 
 
-## choose which signals will be plotted in the waveform window
-# wave_add ...
-
-
-## run the entire simulatio until a $finish or a $stop statement is encountered in testbench
-run all
-
-
-## run the simulation for a certain amoutn of time
-#run 720ns
-#run 2.5us
-
-
-## reset the simulation to t=0
-#restart
