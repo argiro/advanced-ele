1d9035abdbc39a18ba63d8f5c6db236bc54ce66c
Luca Pacher <pacher@to.infn.it>
2016-04-04 10:53:10 +0200

    Update flow.tcl

--- fpga/common/synpnr/scripts/flow.tcl
+++ fpga/common/synpnr/scripts/flow.tcl
@@ -10,8 +10,70 @@
 # [Created]        Mar 23, 2016
 # [Modified]       Mar 29, 2016
 # [Description]    Tcl script to run the complete Xilinx Vivado implementation flow in non-project mode
-# [Notes]          -
+# [Notes]          The script is executed by using
+#
+#                     linux% make flow
+#
 # [Version]        1.0
 # [Revisions]      23.03.2016 - Created
 #-----------------------------------------------------------------------------------------------------
 
+
+## target FPGA (Digilent Arty development board)
+set PART xc7a35ticsg324-1L
+
+
+## HDL sources directory
+set RTL_DIR  $::env(PWD)/../rtl
+
+
+## design constraints directory
+set XDC_DIR  $::env(PWD)/../constraints
+
+
+## scripts directory
+set TCL_DIR $::env(PWD)/scripts
+
+
+## results directory
+set OUT_DIR  $::env(PWD)/results
+
+
+## top-level design module
+set TOP  inverter
+
+
+## list of HDL files
+set RTL_SOURCES [list $RTL_DIR/inverter.v ]
+
+
+## read and parse HDL sources
+read_verilog  $RTL_SOURCES
+
+
+#set_part $PART
+#set_property top $TOP [current_fileset]
+
+
+## read an parse design constraints
+read_xdc  $XDC_DIR/$TOP.xdc
+
+
+## synthesize the design
+synth_design -top $TOP -part $PART
+
+
+## place the design
+place_design
+
+
+## route the design
+route_design
+
+
+## generate bitstream
+write_bitstream -force $OUT_DIR/bitstream/$TOP.bit
+
+
+## optionally, download the bitstream to target FPGA
+source  $TCL_DIR/download.tcl
