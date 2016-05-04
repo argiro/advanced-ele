4805024d87116f7dece76c71b7d76fc733c3baa7
Luca Pacher <pacher@to.infn.it>
2016-05-04 11:01:05 +0200

    added reports and checkpoints

--- fpga/common/synpnr/scripts/flow.tcl
+++ fpga/common/synpnr/scripts/flow.tcl
@@ -8,7 +8,7 @@
 # [Author]         Luca Pacher - pacher@to.infn.it
 # [Language]       Tcl/Xilinx Vivado Tcl commands
 # [Created]        Mar 23, 2016
-# [Modified]       Mar 29, 2016
+# [Modified]       May 04, 2016
 # [Description]    Tcl script to run the complete Xilinx Vivado implementation flow in non-project mode
 # [Notes]          The script is executed by using
 #
@@ -16,20 +16,34 @@
 #
 # [Version]        1.0
 # [Revisions]      23.03.2016 - Created
+#                  04.05.2016 - Added design reporting statements
 #-----------------------------------------------------------------------------------------------------
+
+
+
+###########################################################
+##   RTL SOURCES SETUP  (project-dependent parameters)   ##
+###########################################################
+
 
 ## HDL sources directory
 set RTL_DIR  $::env(PWD)/../rtl
 
-## project-dependent parameters
 
 ## top-level design module
 set TOP  inverter
 
+
 ## list of HDL files
 set RTL_SOURCES [list $RTL_DIR/inverter.v ]
 
-######################################################################################################
+
+
+
+####################################################################
+##   DESIGN  IMPORT  (parse RTL sources and design constraints)   ##
+####################################################################
+
 
 ## target FPGA (Digilent Arty development board)
 set PART xc7a35ticsg324-1L
@@ -47,6 +61,10 @@ set TCL_DIR $::env(PWD)/scripts
 set OUT_DIR  $::env(PWD)/results
 
 
+## reports directory
+set RPT_DIR  $::env(PWD)/reports
+
+
 ## read and parse HDL sources
 read_verilog  $RTL_SOURCES
 
@@ -59,16 +77,70 @@ read_verilog  $RTL_SOURCES
 read_xdc  $XDC_DIR/$TOP.xdc
 
 
+
+
+###################
+##   SYNTHESIS   ##
+###################
+
+
 ## synthesize the design
 synth_design -top $TOP -part $PART
 
+
+## write a database for the synthesized design
+write_checkpoint -force $OUT_DIR/checkpoints/synthesis
+#read_checkpoint -force $OUT_DIR/checkpoints/synthesis
+
+
+## generate post-synthesis reports
+report_utilization -file $RPT_DIR/synthesis/post_syn_utilization.rpt
+report_timing -file $RPT_DIR/synthesis/post_syn_timing.rpt
+
+
+
+
+###################
+##   PLACEMENT   ##
+###################
 
 ## place the design
 place_design
 
 
+## write a database for the routed design
+write_checkpoint -force $OUT_DIR/checkpoints/placement
+
+
+## generate post-routing reports
+report_utilization -file $RPT_DIR/placement/post_placement_utilization.rpt
+report_timing -file $RPT_DIR/placement/post_placement_timing.rpt
+
+
+
+#################
+##   ROUTING   ##
+#################
+
 ## route the design
 route_design
+
+
+## write a database for the routed design
+write_checkpoint -force $OUT_DIR/checkpoints/routing
+
+
+## generate post-routing reports
+report_utilization -file $RPT_DIR/routing/post_routing_utilization.rpt
+report_timing -file $RPT_DIR/routing/post_routing_timing.rpt
+report_power -file $RPT_DIR/routing/post_routing_power.rpt
+report_drc -file $RPT_DIR/routing/post_routing_drc.rpt
+
+
+
+###################
+##   BITSTREAM   ##
+###################
 
 
 ## generate bitstream
@@ -76,4 +148,9 @@ write_bitstream -force $OUT_DIR/bitstream/$TOP.bit
 
 
 ## optionally, download the bitstream to target FPGA
-source  $TCL_DIR/download.tcl
+#source  $TCL_DIR/download.tcl
+
+
+## optionally, start the GUI and debug synthesis/place-and-route results
+#start_gui
+
