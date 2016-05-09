3404423536f02a0433ead7c01853abf861e58e36
Luca Pacher <pacher@to.infn.it>
2016-05-09 11:34:17 +0200

    Update BCD_counter_seg_clk.v

--- fpga/projects/5.BCD_counter/rtl/BCD_counter_seg_clk.v
+++ fpga/projects/5.BCD_counter/rtl/BCD_counter_seg_clk.v
@@ -1,25 +1,42 @@
+
+//-----------------------------------------------------------------------------------------------------
+//                               University of Torino - Department of Physics
+//                                   via Giuria 1 10125, Torino, Italy
+//-----------------------------------------------------------------------------------------------------
+// [Filename]       BCD_counter_seg_clk.v
+// [Project]        Advanced Electronics Laboratory course
+// [Author]         Luca Pacher - pacher@to.infn.it
+// [Language]       Verilog 2001 [IEEE Std. 1364-2001]
+// [Created]        Mar 02, 2016
+// [Modified]       May 09, 2016
+// [Description]    Top-level wrapper for BCD counter and 7-segment display modules
+// [Notes]          Counter input clock mapped to clock divider
+// [Version]        1.0
+// [Revisions]      02.03.2016 - Created
+//-----------------------------------------------------------------------------------------------------
+
  
 `timescale 1ns / 100ps 
  
 // Dependences: 
 // 
 // $RTL_DIR/BCD_counter.v
-// $RTL_DIR/seg_decoder.v 
+// $RTL_DIR/seven_seg_decoder.v 
  
  
 module BCD_counter_seg_clk( 
  
-   input  clk, 
-   input  rst, 
+   input   clk, 
+   input   rst, 
+   output  segA, 
+   output  segB, 
+   output  segC, 
+   output  segD, 
+   output  segE, 
+   output  segF, 
+   output  segG
 
-   output segA, 
-   output segB, 
-   output segC, 
-   output segD, 
-   output segE, 
-   output segF, 
-   output segG, 
-   output segDP ) ;
+   ) ;
 
  
    // clock divider 
@@ -42,7 +59,7 @@ module BCD_counter_seg_clk(
  
    BCD_counter  counter(
  
-      .clk  ( clk_div ), 
+      .clk  ( clk_div ),    // input clock mapped to clock divider
       .rst  (     rst ), 
       .BCD  (     BCD )
  
@@ -52,7 +69,7 @@ module BCD_counter_seg_clk(
 
    // 7-segment decoder
 
-   seg_decoder  decoder(
+   seven_seg_decoder  decoder(
 
       .BCD   (   BCD ),
       .segA  (  segA ),
@@ -62,9 +79,9 @@ module BCD_counter_seg_clk(
       .segE  (  segE ),
       .segF  (  segF ),
       .segG  (  segG ),
-      .segDP ( segDP )
+      .segDP (       )     // leave unconnected
 
    ) ;
- 
-    
+
+
 endmodule 
