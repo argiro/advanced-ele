8349ec1d074fafb630350fbe90baf47ba4aded64
Luca Pacher <pacher@to.infn.it>
2016-05-09 11:33:51 +0200

    Update BCD_counter_seg_PB.v

--- fpga/projects/5.BCD_counter/rtl/BCD_counter_seg_PB.v
+++ fpga/projects/5.BCD_counter/rtl/BCD_counter_seg_PB.v
@@ -1,50 +1,53 @@
+
+
+//-----------------------------------------------------------------------------------------------------
+//                               University of Torino - Department of Physics
+//                                   via Giuria 1 10125, Torino, Italy
+//-----------------------------------------------------------------------------------------------------
+// [Filename]       BCD_counter_seg_PB.v
+// [Project]        Advanced Electronics Laboratory course
+// [Author]         Luca Pacher - pacher@to.infn.it
+// [Language]       Verilog 2001 [IEEE Std. 1364-2001]
+// [Created]        Mar 02, 2016
+// [Modified]       May 09, 2016
+// [Description]    Top-level wrapper for BCD counter and 7-segment display modules
+// [Notes]          Counter input clock mapped to external push-button
+// [Version]        1.0
+// [Revisions]      02.03.2016 - Created
+//-----------------------------------------------------------------------------------------------------
+
  
+
+
 `timescale 1ns / 100ps 
  
 // Dependences: 
 // 
 // $RTL_DIR/BCD_counter.v
-// $RTL_DIR/seg_decoder.v 
- 
-//`define DEBOUNCE 
- 
-module BCD_counter_seg_PB( 
- 
-   input  BTN, 
-   input  rst, 
- 
-   //input  clk, 
-   
-   output segA, 
-   output segB, 
-   output segC, 
-   output segD, 
-   output segE, 
-   output segF, 
-   output segG, 
-   output segDP ) ; 
- 
+// $RTL_DIR/seven_seg_decoder.v
 
+
+module BCD_counter_seg_PB(
  
-   // enable/disable the usage of a single-pulse generator as a debouncer
-    
-   `ifdef DEBOUNCE 
-   reg q0, q1, q2 ; 
-    
-   always @(posedge clk) begin 
-      q0 <= BTN ; 
-      q1 <= q0 ; 
-      q2 <= q0 & (~q1) ; 
-   end 
-   `endif 
-    
+   input   BTN, 
+   input   rst, 
+   output  segA, 
+   output  segB, 
+   output  segC, 
+   output  segD, 
+   output  segE, 
+   output  segF, 
+   output  segG ) ; 
+
+
+
    // BCD counter 
  
-   wire [3:0] BCD ;
+   wire [3:0] BCD ;   // internal 4-bit bus, required to interconnect the BCD counter with the 7-segment display decoder
  
    BCD_counter  counter(
  
-      .clk  (  BTN ), 
+      .clk  (  BTN ),   // input clock mapped to a push-button
       .rst  (  rst ), 
       .BCD  (  BCD )
  
@@ -52,9 +55,9 @@ module BCD_counter_seg_PB(
 
 
 
-   // 7-segment decoder
+   // 7-segment display decoder
 
-   seg_decoder  decoder(
+   seven_seg_decoder  decoder(
 
       .BCD   (   BCD ),
       .segA  (  segA ),
@@ -64,7 +67,7 @@ module BCD_counter_seg_PB(
       .segE  (  segE ),
       .segF  (  segF ),
       .segG  (  segG ),
-      .segDP ( segDP )
+      .segDP (       )     // leave unconnected
 
    ) ;
  
