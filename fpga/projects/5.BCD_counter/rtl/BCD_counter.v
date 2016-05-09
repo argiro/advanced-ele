88a85083575a83776b912e4b8da1a2a51482d439
Luca Pacher <pacher@to.infn.it>
2016-05-09 11:31:28 +0200

    Update BCD_counter.v

--- fpga/projects/5.BCD_counter/rtl/BCD_counter.v
+++ fpga/projects/5.BCD_counter/rtl/BCD_counter.v
@@ -1,4 +1,21 @@
 
+//-----------------------------------------------------------------------------------------------------
+//                               University of Torino - Department of Physics
+//                                   via Giuria 1 10125, Torino, Italy
+//-----------------------------------------------------------------------------------------------------
+// [Filename]       BCD_counter.v
+// [Project]        Advanced Electronics Laboratory course
+// [Author]         Luca Pacher - pacher@to.infn.it
+// [Language]       Verilog 2001 [IEEE Std. 1364-2001]
+// [Created]        Mar 02, 2016
+// [Modified]       May 09, 2016
+// [Description]    Free-running binary-coded decimal (BCD) counter
+// [Notes]          -
+// [Version]        1.0
+// [Revisions]      02.03.2016 - Created
+//-----------------------------------------------------------------------------------------------------
+
+
 `timescale 1ns / 100ps
 
 module BCD_counter(
@@ -8,15 +25,19 @@ module BCD_counter(
    output reg [3:0] BCD ) ;
    
 
-   always @(posedge clk or posedge rst) begin
-      if( rst == 1'b1 )           // asynchronous reset !
+   always @(posedge clk) begin 	                            // synchronous reset
+   //always @(posedge clk or posedge rst) begin             // asynchronous reset
+
+      if( rst == 1'b1 )
          BCD <= 4'b0000 ;
+
       else begin
+
          if( BCD == 4'b1001 )     // force the count roll-over at 9
             BCD <= 4'b0000 ;
          else
-            BCD <= BCD + 1 ;
+            BCD <= BCD + 4'b0001 ;
       end
-   end
+   end // always
 
 endmodule
