7834b9b7843a754fd9d59d88e2dc5ca7a785806a
Luca Pacher <pacher@to.infn.it>
2016-05-09 11:36:52 +0200

    Update BCD_counter_Ndigit.v

--- fpga/projects/6.Ndigit_7seg_display/rtl/BCD_counter_Ndigit.v
+++ fpga/projects/6.Ndigit_7seg_display/rtl/BCD_counter_Ndigit.v
@@ -1,4 +1,21 @@
 
+//-----------------------------------------------------------------------------------------------------
+//                               University of Torino - Department of Physics
+//                                   via Giuria 1 10125, Torino, Italy
+//-----------------------------------------------------------------------------------------------------
+// [Filename]       BCD_counter_Ndigit.v
+// [Project]        Advanced Electronics Laboratory course
+// [Author]         Luca Pacher - pacher@to.infn.it
+// [Language]       Verilog 2001 [IEEE Std. 1364-2001]
+// [Created]        Mar 02, 2016
+// [Modified]       May 09, 2016
+// [Description]    Parameterizable N-digit BCD counter
+// [Notes]          -
+// [Version]        1.0
+// [Revisions]      02.03.2016 - Created
+//-----------------------------------------------------------------------------------------------------
+
+
 `timescale 1ns / 100ps
 
 
@@ -10,12 +27,12 @@
 
 module BCD_counter_Ndigit(clk, rst, en, BCD) ;
 
-   parameter Ndigit = 3 ;
+   parameter  Ndigit = 3 ;
 
-   input clk ;
-   input rst ;
-   input en ;
-   output [Ndigit*4-1:0] BCD ;
+   input   clk ;
+   input   rst ;
+   input   en ;
+   output  [Ndigit*4-1:0] BCD ;
 
 
 
