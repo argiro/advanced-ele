7c6096835fb220810987cbf5a872ae48cacc0d7c
Luca Pacher <pacher@to.infn.it>
2016-05-09 11:46:11 +0200

    Update tb_BCD_counter.v

--- fpga/projects/5.BCD_counter/testbench/tb_BCD_counter.v
+++ fpga/projects/5.BCD_counter/testbench/tb_BCD_counter.v
@@ -1,4 +1,21 @@
 
+//-----------------------------------------------------------------------------------------------------
+//                               University of Torino - Department of Physics
+//                                   via Giuria 1 10125, Torino, Italy
+//-----------------------------------------------------------------------------------------------------
+// [Filename]       tb_BCD_counter.v [TESTBENCH]
+// [Project]        Advanced Electronics Laboratory course
+// [Author]         Luca Pacher - pacher@to.infn.it
+// [Language]       Verilog 2001 [IEEE Std. 1364-2001]
+// [Created]        Mar 02, 2016
+// [Modified]       May 09, 2016
+// [Description]    Testbench for a single BCD counter
+// [Notes]          -
+// [Version]        1.0
+// [Revisions]      02.03.2016 - Created
+//-----------------------------------------------------------------------------------------------------
+
+
 
 `timescale 1ns / 100ps
 
@@ -14,6 +31,7 @@ module tb_BCD_counter ;
 
    
    BCD_counter   DUT(
+
       .clk (  clk ),
       .rst (  rst ),
       .BCD (  LED )
