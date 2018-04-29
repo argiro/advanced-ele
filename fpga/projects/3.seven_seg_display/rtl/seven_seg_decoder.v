0e91c4af9064804b5293970ab324f614ee7d4bb6
Luca Pacher <pacher@to.infn.it>
2018-04-29 22:24:17 +0200

    More up to students

--- fpga/projects/3.seven_seg_display/rtl/seven_seg_decoder.v
+++ fpga/projects/3.seven_seg_display/rtl/seven_seg_decoder.v
@@ -5,19 +5,18 @@
 //-----------------------------------------------------------------------------------------------------
 // [Filename]       seven_seg_decoder.v
 // [Project]        Advanced Electronics Laboratory course
-// [Author]         Luca Pacher - pacher@to.infn.it
+// [Author]         <group>
 // [Language]       Verilog 2001 [IEEE Std. 1364-1995]
-// [Created]        Mar 16, 2016
-// [Modified]       Mar 16, 2016
-// [Description]    BCD to 7-segments display decoder 
-// [Notes]          Available 7-segments display modules are COMMON ANODE devices
+// [Created]        -
+// [Modified]       -
+// [Description]    Complete the following code-skeleton to implement a BCD to 7-segments display
+//                  decoder.
+//
+// [Notes]          Available 7-segments display modules are **COMMON ANODE** devices.
+//
 // [Version]        1.0
-// [Revisions]      16.03.2016 - Created
+// [Revisions]      -
 //-----------------------------------------------------------------------------------------------------
-
-
-
-
 
 `timescale 1ns / 100ps
 
@@ -38,19 +37,19 @@ module seg_decoder(
    always @(*) begin
    
       case(BCD)
-                                                                        //  abcdefg-
-         4'b0000  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b00000011 ;  //  0
-         4'b0001  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b10011111 ;  //  1
-         4'b0010  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b00100101 ;  //  2
-         4'b0011  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b00001101 ;  //  3
-         4'b0100  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b10011001 ;  //  4
-         4'b0101  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b01001001 ;  //  5
-         4'b0110  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b01000001 ;  //  6
-         4'b0111  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b00011111 ;  //  7
-         4'b1000  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b00000001 ;  //  8
-         4'b1001  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b00001001 ;  //  9
-         
-         default  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b11111101 ;  // minus sign otherwise
+                                                                        //  a b c d e f g DP
+         4'b0000  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b0_0_0_0_0_0_1_1 ;  //  0
+         4'b0001  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = ...                ;  //  1
+         4'b0010  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = ...                ;  //  2
+         4'b0011  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = ...                ;  //  3
+         4'b0100  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = ...                ;  //  4
+         4'b0101  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = ...                ;  //  5
+         4'b0110  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = ...                ;  //  6
+         4'b0111  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = ...                ;  //  7
+         4'b1000  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = ...                ;  //  8
+         4'b1001  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = ...                ;  //  9
+
+         default  :  {segA, segB, segC, segD, segE, segF, segG, segDP} = 8'b11111101 ; // minus sign otherwise
 
       endcase
    end
